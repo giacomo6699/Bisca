bb7520ebd9a29ac94e57695c8493992e2e783e2b
Giacomo Seno <giacomo6699@gmail.com>
2020-07-20 12:10:29 +0200

    Inizio Archivio Vincite

--- app/src/main/java/com/example/bisca/Vincitore.java
+++ app/src/main/java/com/example/bisca/Vincitore.java
@@ -1,5 +1,6 @@
 package com.example.bisca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +10,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Vincitore extends AppCompatActivity {
+    DatabaseReference DATARef;
+    DataSnapshot DATABASE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vincitore);
+        DATARef = FirebaseDatabase.getInstance().getReference();
+        DATARef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                DATABASE = dataSnapshot;
+                int size = Integer.parseInt(DATABASE.child("Vincitori").child("Size").getValue().toString());
+                Iterable<DataSnapshot> children = DATABASE.child("Vincitori").getChildren();
+                for (DataSnapshot data : children){
+                    if (!data.getKey().equals("Size")){
+                        //allora aggiungi alla lista il nome del giocatore (data.getKey()) e le sue vincite (data.getValue())
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         String nome = getIntent().getExtras().getString("Nome");
         TextView tv = findViewById(R.id.TVVincitore);
         getSupportActionBar().hide();
